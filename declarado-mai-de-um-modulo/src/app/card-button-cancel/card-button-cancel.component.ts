af2d763f2cc541b57097bd330817cb54d0099c77
Ranys Morais <ranys.morais@brgaap.com>
2025-04-08 21:51:49 -0300

    aplicando host e ng-deep

--- declarado-mai-de-um-modulo/src/app/card-button-cancel/card-button-cancel.component.ts
+++ declarado-mai-de-um-modulo/src/app/card-button-cancel/card-button-cancel.component.ts
@@ -4,21 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-card-button-cancel',
   standalone: false,
   templateUrl: './card-button-cancel.component.html',
-  styleUrls: ['./card-button-cancel.component.scss', './card-button-cancel-2.component.scss'],
-  // styles: [
-  //   ` .card-cancel-button {
-  //     padding: 10px;
-  //     font-weight: bold;
-  //     font-size: 16px;
-  //     text-align: center;
-  //     margin-top: 10px;
-  //     border-radius: 25px;
-  //     background-color: rgb(255, 2, 2);
-  //     color: white;
-  //     cursor: pointer;
-  // }
-  //  `
-  //]
+  styleUrl: './card-button-cancel.component.scss',
 })
 export class CardButtonCancelComponent {
 
