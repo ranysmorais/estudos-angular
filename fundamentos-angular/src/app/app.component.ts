b8bb5e97797747cdb4df7b3ff4b65ca3cc22f58d
Ranys Morais <ranysmorais@hotmail.com>
2025-05-01 02:00:08 -0300

    bindings

--- fundamentos-angular/src/app/app.component.ts
+++ fundamentos-angular/src/app/app.component.ts
@@ -1,12 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'fundamentos-angular';
+  isDisabled = true;
+  showPaasswordInput = false;
+  passwordInputType = 'password';
+  passwordInputTypeObj = { password: 'password', text: 'text' };
+
+
+  enableInput() {
+    this.isDisabled = false;
+  }
+  disabledInput() {
+    this.isDisabled = true;
+  }
+
+  showPassword() {
+    this.showPaasswordInput = !this.showPaasswordInput;
+
+    const inputType = !this.showPaasswordInput ?
+      this.passwordInputTypeObj.password
+      : this.passwordInputTypeObj.text;
+
+    this.passwordInputType = inputType;
+  }
+
 }
